05115cf3fe195035c8ea14af9d038a66b9fc344c
Benoist LAURENT <benoist.laurent@gmail.com>
2016-06-16 16:48:04 +0200

    Bump version: 1.1.0 → 1.1.1

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ requirements = [
 
 
 setup(name='dock2nc',
-      version='1.1.0',
+      version='1.1.1',
       description='Convert ASCII docking files to netCDF.',
       long_description=readme + '\n\n' + history,
       author="Benoist LAURENT",
